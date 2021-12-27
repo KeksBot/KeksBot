e890eafca2f57811cb4ab7a8cf324e2408ee80a6
Clyde | Netzwerkpolizei™️ <74925250+FixlTV@users.noreply.github.com>
2021-12-27 11:26:30 +0100

    debugging zeug

--- events/tempban.js
+++ events/tempban.js
@@ -10,7 +10,7 @@ module.exports = {
         for await (const data of serverdata.find({ tempbans: { $exists: true } })) {
             var guild
             try { guild = await client.guilds.fetch(data._id) } catch {
-                try { await serverdata.findByIdAndDelete(data._id) } catch {}
+                try { await serverdata.findByIdAndDelete(data._id) } catch (err) {console.error(err)}
             }
             if(guild) {
                 let tempbans = [...data.tempbans]
@@ -26,25 +26,32 @@ module.exports = {
                 })
                 if(tempbans !== data.tempbans) await update('serverdata', data, { tempbans: data.tempbans })
             } else {
-                try { await serverdata.findByIdAndDelete(data._id) } catch {}
+                try { await serverdata.findByIdAndDelete(data._id) } catch (err) {console.error(err)}
             }
         }
         setTimeout(async function() {
             await require('../db/database')()
             for await (const data of serverdata.find({ tempbans: { $exists: true } })) {
-                let guild = await client.guilds.fetch(data._id)
-                let tempbans = [...data.tempbans]
-                data.tempbans.forEach(async function(ban) {
-                    if(ban.time <= Date.now()) {
-                        try {
-                            if(await guild.bans.fetch(ban.user)) {
-                                await guild.bans.remove(ban.user)
-                                data.tempbans.splice(data.tempbans.indexOf(ban), 1)
-                            }
-                        } catch {}
-                    }
-                })
-                if(tempbans !== data.tempbans) await update('serverdata', data, { tempbans: data.tempbans })
+                var guild
+                try { guild = await client.guilds.fetch(data._id) } catch {
+                    try { await serverdata.findByIdAndDelete(data._id) } catch (err) {console.error(err)}
+                }
+                if(guild) {
+                    let tempbans = [...data.tempbans]
+                    data.tempbans.forEach(async function(ban) {
+                        if(ban.time <= Date.now()) {
+                            try {
+                                if(await guild.bans.fetch(ban.user)) {
+                                    await guild.bans.remove(ban.user)
+                                    data.tempbans.splice(data.tempbans.indexOf(ban), 1)
+                                }
+                            } catch {}
+                        }
+                    })
+                    if(tempbans !== data.tempbans) await update('serverdata', data, { tempbans: data.tempbans })
+                } else {
+                    try { await serverdata.findByIdAndDelete(data._id) } catch (err) {console.error(err)}
+                }
             }
         }, 300000)
     }
