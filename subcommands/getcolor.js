aabcee23f47bf64e53151262b43b14852fa1da2d
Clyde | Netzwerkpolizei™️ <74925250+FixlTV@users.noreply.github.com>
2022-01-09 14:48:51 +0100

    Neues Farbschema als Standard festgelegt

--- subcommands/getcolor.js
+++ subcommands/getcolor.js
@@ -7,17 +7,17 @@ const { Guild } = require("discord.js")
  */
 
 module.exports = async (guild) => {
-    if(!guild) return { red: 0xff0000, lightblue: 0x3498db, lime: 0x2ecc71, yellow: 0xf1c40f, normal: 0x00b99b }
+    if(!guild) return { red: 0xE62535, yellow: 0xF2E03F, lime: 0x25D971, normal: 0x00b99b }
     if(!guild.data) guild.data = await require('../db/getData')('serverdata', guild.id)
     if(guild.data?.theme) {
-        let {
-            red = 0xe62535,
-            lightblue = 0x3498db,
-            lime = 0x25d971,
-            yellow = 0xf2e03f,
+        let { 
+            red = 0xE62535, 
+            yellow = 0xF2E03F, 
+            lime = 0x25D971,
             normal = 0x00b99b
         } = guild.data.theme
+
         if(normal == 'role') normal = guild.me?.displayHexColor || 0x00b99b
         return { red, yellow, lime, normal, lightblue }
-    } else return { red: 0xff0000, lightblue: 0x3498db, lime: 0x2ecc71, yellow: 0xf1c40f, normal: 0x00b99b }
+    } else return { red: 0xE62535, yellow: 0xF2E03F, lime: 0x25D971, normal: 0x00b99b }
 }
