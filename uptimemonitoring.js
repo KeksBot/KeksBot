d8cf3fa92b19c1603454c3775526d350923f6e1d
Clyde | Netzwerkpolizei™️ <74925250+FixlTV@users.noreply.github.com>
2022-01-11 11:59:26 +0100

    possible bugfix for monitoring errors

--- uptimemonitoring.js
+++ uptimemonitoring.js
@@ -4,6 +4,6 @@ module.exports = function uptimemonitoring(uptimeurl, client) {
     https.get(uptimeurl.replace('PING', client.ws.ping))
     setInterval(() => {
         https.get(uptimeurl.replace('PING', client.ws.ping))
-    }, 1000 * 30)
+    }, 1000 * 60)
 
 }
