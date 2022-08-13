25f7cc548f9a4f8ccd9f8cec5af702280f4233c0
Clyde | Netzwerkpolizei™️ <74925250+FixlTV@users.noreply.github.com>
2022-08-13 15:20:28 +0200

    errorhanding

---
+++ src/subcommands/handleError.ts
@@ -0,0 +1,49 @@
+import { ButtonInteraction, CommandInteraction, EmbedBuilder, ModalSubmitInteraction, SelectMenuInteraction } from "Discord.js";
+import { logChannel } from '../config.json'
+
+export default async (interaction: CommandInteraction | ButtonInteraction | ModalSubmitInteraction | SelectMenuInteraction, error: unknown, args?: any) => {
+    try {
+        //@ts-ignore
+        console.error(`Error while executing ${interaction?.commandName || interaction?.customId} (${interaction?.user?.tag}, ${interaction?.guild?.name}) with arguments: ${args.replaceAll('"', '')}`)
+        console.error(error)
+        let errorId = (Math.random() * Date.now()).toString(36)
+        let channel = await interaction.client.channels.fetch(logChannel)
+        let embed = new EmbedBuilder() 
+            .setColor('Red')
+            //@ts-ignore
+            .setTitle(`${errorId} | ${error?.name}`)
+            //@ts-ignore
+            .setDescription(`**interaction**:\ncommand: /${interaction?.commandName || interaction?.customId}\nuser: <@${interaction?.user?.id}> (${interaction?.user?.id})\nguild: ${interaction?.guild?.name} (${interaction?.guild?.id})`)
+            .addFields([
+                {
+                    name: 'args',
+                    value: args.replaceAll('"', '') || 'undefined',
+                    inline: true
+                },
+                {
+                    name: 'error',
+                    //@ts-ignore
+                    value: error.message || 'undefined',
+                    inline: true
+                },
+                {
+                    name: 'cause',
+                    //@ts-ignore
+                    value: error.cause.toString() || 'undefined',
+                },
+                {
+                    name: 'stack',
+                    //@ts-ignore
+                    value: error.stack || 'undefined',
+                }
+            ])
+        //@ts-ignore
+        await channel.send({ embeds: [embed] })
+        embed = new EmbedBuilder()
+            .setColor(interaction?.color?.red || 'Red')
+            .setTitle(`Fehler`)
+            .setDescription(`Ein Fehler ist aufgetreten und der Command konnte nicht ordnungsgemäß ausgeführt werden.\nFehler-Code: ${errorId}`)
+        await interaction.followUp({ embeds: [embed], ephemeral: true })
+    } catch (error) {console.error}
+
+}
