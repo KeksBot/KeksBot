8a49b4b7d400d7891ec222bc5677497990f7359f
Clyde | Netzwerkpolizei™️ <74925250+FixlTV@users.noreply.github.com>
2022-10-01 11:54:25 +0200

    spezi buff

--- src/game/gameobjects/1spezi.ts
+++ src/game/gameobjects/1spezi.ts
@@ -2,9 +2,9 @@ const obj: BattleActionBuilder = {
     id: 1,
     name: 'Spezi',
     type: 'item/med',
-    description: 'Das einzigwahre Getränk dieser Welt. Stellt bei Konsum 500 HP wieder her und schmeckt außerdem noch gut',
+    description: 'Das einzigwahre Getränk dieser Welt. Stellt bei Konsum 75% der maximalen HP wieder her und schmeckt außerdem noch gut',
     priority: 8,
-    aHeal: { value: 500 },
+    rHeal: { value: 75 },
     fightUsable: true,
     usageMessage: '{user} trinkt eine Flasche Spezi'
 }
