847c0cd7f99bda7be2d4b752a5363d628c686462
Clyde | Netzwerkpolizei™️ <74925250+FixlTV@users.noreply.github.com>
2022-10-28 23:17:33 +0200

    /item metadata support

--- src/autocomplete/itemSelection.ts
+++ src/autocomplete/itemSelection.ts
@@ -15,9 +15,12 @@ const options: AutocompleteOptions = {
         if(input.length < 3) return interaction.respond([])
 
         //@ts-ignore
-        const items: Discord.Collection<number, BattleActionBuilder & { count: number }> = new Discord.Collection(objectLoader(inventory.map(item => item.id)).entries())
-        items.forEach(item => {
-            item.count = inventory.find(i => i.id === item.id)?.count || 0
+        const items: BattleAction[] = inventory.map((i, index) => {
+            //@ts-ignore
+            let item = Object.assign({...i._doc}, objectLoader(inventory.map(item => item.id)).get(i.id))
+            item.onLoad && item.onLoad.call(item)
+            item.index = index
+            return item
         })
 
         let results = items.filter(item => item.name.toLowerCase().includes(input)).sort((a, b) => {
@@ -25,14 +28,15 @@ const options: AutocompleteOptions = {
             if(a.name.toLowerCase() != input && b.name.toLowerCase() == input) return 1
             if(a.name.toLowerCase().startsWith(input) && !b.name.toLowerCase().startsWith(input)) return -1
             if(!a.name.toLowerCase().startsWith(input) && b.name.toLowerCase().startsWith(input)) return 1
-        }).array().slice(0, 25)
+        }).slice(0, 25)
 
         // let firstResults = items.filter(i => i.name.toLowerCase() == input).array()
         // let secondResults = items.filter(i => i.name.toLowerCase().startsWith(input)).array().filter(i => !firstResults.includes(i))
         // let thirdResults = items.filter(i => i.name.toLowerCase().includes(input)).array().filter(i => !firstResults.includes(i) && !secondResults.includes(i))
 
         // let output = [firstResults, secondResults, thirdResults].flat().map(i => { return { name: i.name, value: i.id.toString() } }).slice(0, 25)
-        let output = results.map(i => { return { name: i.name, value: i.id.toString() } })
+        //@ts-ignore
+        let output = results.map((i) => { return { name: i.name, value: i.index.toString() } })
 
         await interaction.respond(output)
     }
--- src/slashcommands/rp/item.ts
+++ src/slashcommands/rp/item.ts
@@ -17,10 +17,12 @@ const options: CommandOptions = {
     ],
     execute: async function (interaction: Discord.CommandInteraction | Discord.ButtonInteraction, args, client) {
         const { user, color } = interaction
-        const id = args.item
+        const index = args.item
 
-        let item: BattleAction = objectLoader([id]).get(id) || {}
-        item.count = user.data.inventory?.find(i => i.id === id)?.count || 0
+        //@ts-ignore
+        let item: BattleAction = {...user.data.inventory[index]._doc}
+        item = Object.assign(item, objectLoader([item.id]).get(item.id))
+        item.onLoad && item.onLoad()
 
         if(!item.name) return interaction.error('Unbekanntes Item', 'Dieses Item scheint nicht zu existieren', true)
         if(!item.count) return interaction.error('Keine Items', 'Du hast dieses Item nicht', true)
@@ -38,7 +40,7 @@ const options: CommandOptions = {
         let button = new Discord.ActionRowBuilder<Discord.ButtonBuilder>()
             .addComponents(
                 new Discord.ButtonBuilder()
-                    .setCustomId('item_use:' + id)
+                    .setCustomId('item_use:' + index)
                     .setLabel('Einsetzen')
                     .setStyle(Discord.ButtonStyle.Primary)
                     .setDisabled(!item.inventoryUsable)
@@ -46,25 +48,26 @@ const options: CommandOptions = {
         
         let res = await interaction.reply({ embeds: [embed], components: [button], ephemeral: true })
 
-        interaction = await res.awaitMessageComponent({ componentType: Discord.ComponentType.Button, time: 120000 }).catch(() => null)
+        interaction = await res.awaitMessageComponent({ componentType: Discord.ComponentType.Button, time: 300000 }).catch(() => null)
         while(interaction) {
             interaction.color = color
             //@ts-ignore
             if(!item.count) return interaction.error('Keine Items', 'Du hast dieses Item nicht', true)
-            let output = true
+            let output: any = true
             if(item.onInvUse) {
                 //@ts-ignore
-                output = await item.onInvUse(item, interaction, user)
+                output = await item.onInvUse(item, user, interaction)
+                console.log(output)
             }
             let embed
-            if(output) {
+            if(output && (!output?.length || output?.[0])) {
                 if(item.aHeal) user.data.battle.currentHP += item.aHeal.value
                 if(item.rHeal) user.data.battle.currentHP += Math.round(user.data.battle.skills.find(s => s.name == 'HP').value * item.rHeal.value)
                 if(user.data.battle.currentHP > user.data.battle.skills.find(s => s.name == 'HP').value) user.data.battle.currentHP = user.data.battle.skills.find(s => s.name == 'HP').value
                 // TODO: Stat modifiers
                 item.count --
-                user.data.inventory.find(i => i.id == id).count --
-                if(user.data.inventory.find(i => i.id == id).count <= 0) user.data.inventory.splice(user.data.inventory.findIndex(i => i.id === id), 1)
+                user.data.inventory[index].count --
+                if(user.data.inventory[index].count <= 0) user.data.inventory.splice(user.data.inventory.findIndex(i => i.id === index), 1)
                 await user.save()
                 embed = new Discord.EmbedBuilder()
                     .setColor(color.normal)
@@ -78,7 +81,7 @@ const options: CommandOptions = {
                 //@ts-ignore
                 .setTitle(item.emote ? `${emotes.items[item.emote] || '[ ]'} ${item.name.title()}` : `[ ] ${item.name.title()}`)
                 .setDescription(`Anzahl: **${item.count}**\nTyp: **${type}**\n${item.description}` || `Anzahl: **${item.count}**\nTyp: **${type}**\nKeine Beschreibung verfügbar`)
-                .setFooter({ text: 'Das Item konnte nicht benutzt werden.' })
+                .setFooter({ text: output == null ? 'Der Vorgang wurde abgebrochen' : output?.length && !output[0] ? output[1] : 'Das Item konnte nicht benutzt werden' })
 
             //@ts-ignore
             await interaction.update({ embeds: [embed], components: [button] })
