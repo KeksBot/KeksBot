54ddb5e1abe016992194c51cbf5edc9baf8c8611
Clyde | Netzwerkpolizei™️ <74925250+FixlTV@users.noreply.github.com>
2023-01-29 17:51:07 +0100

    getData stuff idk

--- src/db/getData.ts
+++ src/db/getData.ts
@@ -18,18 +18,18 @@ const prisma = new PrismaClient()
 
 async function get(schema: 'user' | 'server', id: string, modules?: DbSchemas): Promise<any> {
     const include: any = {}
-    modules.forEach(m => {
+    modules?.forEach(m => {
         if(m.includes('/')) {
             if(typeof include[m.split('/')[0].replaceAll(/user|server/g, '')] !== 'object') include[m.split('/')[0].replaceAll(/user|server/g, '')] = { include: {} } //Nur falls Items irgendwann einzeln gespeichert werden
             include[m.split('/')[0].replaceAll(/user|server/g, '')].include[m.split('/')[1].replaceAll(/user|server/g, '')] = true
         } else !include[m.replaceAll(/user|server/g, '')] ? include[m.replaceAll(/user|server/g, '')] = true : null
     })
-    include.loggedInAs = modules.length ? { include } : true
+    if(schema == 'user') include.loggedInAs = modules?.length ? { include } : true
     const options: any = {
         where: {
             id
         },
-        include
+        include: Object.keys(include).length ? include : undefined
     } //@ts-ignore
     let data = await prisma[schema].findUnique(options)
     // TODO: handle
